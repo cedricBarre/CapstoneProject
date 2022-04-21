b3b7fca05d97f6cd9515970018d3431132389fee
jedlamari <jed.lamari-saysset@mail.mcgill.ca>
2022-04-21 11:34:04 -0400

    added shell script

--- noiseDistribution.sh
+++ noiseDistribution.sh
@@ -1,27 +1,33 @@
-#!/bin/bash
 module load minc-toolkit
-
-reference_image="$1" #input
-minc_files_path = "$2"
-output_path="$3" #output
-upper_threshold = 19 
-
-
-# Transform each nifti file and create 
-scan_name="$(basename -- $reference_image)" # Extract basename
-scan_name="${scan_name%%.*}" # Remove nifti extension
-minc_file="$original_file.mnc" # Remove nifti extension
-nii2mnc -noscanrange $reference_image ./$minc_files_path/$minc_file
-
-mincmath -segment -const2 0 upper_threshold original_file.mnc binarized_file.mnc
-mincmorph -erosion binarized_file.mnc binarized_eroded_file.mnc
-
-
-mnc2nii $output_path/binarized_eroded_file.mnc $output_path/binarized_eroded_file.nii
-   gzip $output_path/binarized_eroded_file.nii
-   output_file="$output_path/binarized_eroded_file.mnc"
-
-
-
-
-
+minc_scan=$1 #input
+output_path=$2 #output
+#Using the mincmath
+mincmath -segment -const2 0 19 $minc_scan $output_path/binarized_file.mnc
+#First Erosion
+mincmorph -erosion ./binarized_file.mnc ./binarized_eroded_file01.mnc
+#Transforming the minc file in nifti of the first erosion
+mnc2nii $output_path/binarized_eroded_file01.mnc $output_path/binarized_eroded_file01.nii
+gzip $output_path/binarized_eroded_file01.nii
+#Second Erosion
+mincmorph -erosion $output_path/binarized_eroded_file01.mnc $output_path/binarized_eroded_file02.mnc
+#Transforming the minc file in nifti of the second erosion
+mnc2nii $output_path/binarized_eroded_file02.mnc $output_path/binarized_eroded_file02.nii
+gzip $output_path/binarized_eroded_file02.nii
+#Thrid erosion
+mincmorph -erosion $output_path/binarized_eroded_file02.mnc $output_path/binarized_eroded_file03.mnc
+#Transforming the minc file in nifti of the third erosion
+mnc2nii $output_path/binarized_eroded_file03.mnc $output_path/binarized_eroded_file03.nii
+gzip $output_path/binarized_eroded_file03.nii
+#Fourth Erosion
+mincmorph -erosion $output_path/binarized_eroded_file03.mnc $output_path/binarized_eroded_file04.mnc
+#Transforming the minc file in nifti of the fourth erosion
+mnc2nii $output_path/binarized_eroded_file04.mnc $output_path/binarized_eroded_file04.nii
+gzip $output_path/binarized_eroded_file04.nii
+#Fift Erosion
+mincmorph -erosion $output_path/binarized_eroded_file04.mnc $output_path/binarized_eroded_file05.mnc
+#Transforming the minc file in nifti of the fourth erosion
+mnc2nii $output_path/binarized_eroded_file05.mnc $output_path/binarized_eroded_file05.nii
+gzip $output_path/binarized_eroded_file05.nii
+for file in /data/scratch/jed/noise_distribution/*
+do echo "\r files: ${file##*/}"
+done
