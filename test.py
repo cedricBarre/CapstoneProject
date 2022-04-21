1a64ad41cb708838ba851f6327e07b0f56e5a651
Cédric Barré -- Chakravarty Honours Student <barced@cicws09.douglas.rtss.qc.ca>
2022-04-21 13:46:02 -0400

    Final script done

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-MINC_FILES_DIR = "output/minc_files"
-
-if not os.path.exists(MINC_FILES_DIR):
-    os.makedirs(MINC_FILES_DIR)
-shutil.copy('scans.csv', MINC_FILES_DIR + "/101/")
