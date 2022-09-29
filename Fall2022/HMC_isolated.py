42567f380d2cf0de763d6ec0d6c7ed7ff4ceb31c
Cedric Barre <cedbarr@gmail.com>
2022-09-29 16:24:39 -0400

    Added folder restructuring

---
+++ Fall2022/HMC_isolated.py
@@ -0,0 +1,15 @@
+'''
+     _   _  __  __  ___    ____  ___  _____  __      __   ____  ____  ____  
+    ( )_( )(  \/  )/ __)  (_  _)/ __)(  _  )(  )    /__\ (_  _)( ___)(  _ \ 
+     ) _ (  )    (( (__    _)(_ \__ \ )(_)(  )(__  /(__)\  )(   )__)  )(_) )
+    (_) (_)(_/\/\_)\___)  (____)(___/(_____)(____)(__)(__)(__) (____)(____/ 
+
+    Python script used to run the ANTs motion correction algorithm in isolation
+    from the RABIES preprocessing pipeline. This script will allow us to excite 
+    the ANTs algorithm with various datasets and obtain the raw output to evaluate 
+    relationships between specific parameters in the input datasets and the effects
+    on the outputs. 
+'''
+
+if __name__ == "__main__":
+    print("Running HMC in isolation...")
