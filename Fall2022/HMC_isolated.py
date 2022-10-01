446d8cebbc9f7a62c7ae40b389e3e3dc7beb7e45
Cedric Barre <cedbarr@gmail.com>
2022-09-30 20:51:33 -0400

    Started development on the python script

--- Fall2022/HMC_isolated.py
+++ Fall2022/HMC_isolated.py
@@ -11,5 +11,32 @@
     on the outputs. 
 '''
 
+import subprocess, argparse
+import SimpleITK as sitk
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='Head motion correction stage of RABIES pipeline preprocessing stage')
+    parser.add_argument('moving', 
+                        help='Name of moving 3D scan with temporal dimension')
+    parser.add_argument('reference', 
+                        help='Name of reference 3D scan')
+    parser.add_argument('output', 
+                        help='Output folder')
+
+    return parser.parse_args()
+
+def executeANTsMotionCorr(moving, reference, output):
+    print(f"Moving input = {moving}, reference input = {reference} and output folder = {output}")
+
+    moving_img = sitk.ReadImage(moving)
+    n = moving_img.GetSize()[3]
+    if (n > 10):
+        n = 10
+    command = f"antsMotionCorr -d 3 -o [{output}/motcorr,{output}/motcorr_warped.nii.gz,{output}/motcorr_avg.nii.gz] -m MI[ {reference} , \
+                {moving} , 1 , 20 , regular, 0.2 ] -t Rigid[ 0.25 ] -i 50x20 -s 1x0 -f 2x1 -u 1 -e 1 -l 1 -n {str(n)}"
+
 if __name__ == "__main__":
     print("Running HMC in isolation...")
+    args = parseArguments()
+
+    executeANTsMotionCorr(args.moving, args.reference, args.output)
--- Fall2022/run_hmc.sh
+++ Fall2022/run_hmc.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
 
-singularity exec --bind ./:/mnt/ ../../Fall2022/rabies.sif python /mnt/HMC_isolated.py
+POSITIONAL_ARGS=()
+MOVING=""
+OUTPUT=""
+REFERENCE=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -m|--moving)
+      MOVING="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--output)
+      OUTPUT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -r|--reference)
+      REFERENCE="$2"
+      shift # past argument
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [ "$MOVING" == "" ]; then
+    echo Failed to provide a moving image
+    exit 1
+fi
+if [ "$OUTPUT" == "" ]; then
+    echo Failed to provide an output folder
+    exit 1
+fi
+if [ "$REFERENCE" == "" ]; then
+    echo Failed to provide a reference image
+    exit 1
+fi
+
+singularity exec --bind ./HMC_isolated.py:/mnt/HMC_isolated.py \
+                 --bind $MOVING:/mnt/moving.nii.gz \
+                 --bind $REFERENCE:/mnt/reference.nii.gz \
+                 --bind $OUTPUT:/mnt/output \
+                 ../../Fall2022/rabies.sif \
+                 python /mnt/HMC_isolated.py /mnt/moving.nii.gz /mnt/reference.nii.gz /mnt/output 
 
 #singularity exec --bind ./:/mnt/ ../../Fall2022/rabies.sif antsMotionCorr --help
