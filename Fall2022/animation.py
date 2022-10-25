2fec4c4de0edac74e72549f03c6f4d7987b6083d
Jed Lamari-Saysset <jed.lamari-saysset@mail.mcgill.ca>
2022-10-25 14:53:58 -0400

    Update animation.py

--- Fall2022/animation.py
+++ Fall2022/animation.py
@@ -58,3 +58,62 @@ def animation(QPP_reshaped):
     HTML(anim.to_html5_video())
 
     #for IPython we shall pip install IPython
+    
+    
+    #from rabies.visualisation
+def plot_3d(axes,sitk_img,fig,vmin=0,vmax=1,cmap='gray', alpha=1, cbar=False, threshold=None, planes=('sagittal', 'coronal', 'horizontal'), num_slices=4, slice_spacing=0.1):
+    physical_dimensions = (np.array(sitk_img.GetSpacing())*np.array(sitk_img.GetSize()))[::-1] # invert because the array is inverted indices
+    array=sitk.GetArrayFromImage(sitk_img)
+
+    array[array==0]=None # set 0 values to be empty
+
+    if not threshold is None:
+        array[np.abs(array)<threshold]=None
+
+    slice_0 = (1.0-((num_slices-1)*slice_spacing))/2
+    slice_fractions=[slice_0]
+    for i in range(1,num_slices):
+        slice_fractions.append(slice_0+(i*slice_spacing))
+
+    cbar_list = []
+    
+    ax_number=0
+    if 'sagittal' in planes:
+        ax=axes[ax_number]
+        ax_number+=1
+        empty_slice = np.array([np.nan]).repeat(array.shape[0])[:,np.newaxis]
+        slices=empty_slice
+        for s in slice_fractions:
+            slice=array[::-1,:,int(array.shape[2]*s)]
+            slices=np.concatenate((slices,slice,empty_slice),axis=1)
+        pos = ax.imshow(slices, extent=[0,physical_dimensions[1]*num_slices,0,physical_dimensions[0]], vmin=vmin, vmax=vmax,cmap=cmap, alpha=alpha, interpolation='none')
+        ax.axis('off')
+        if cbar:
+            cbar_list.append(fig.colorbar(pos, ax=ax))
+
+    if 'coronal' in planes:
+        ax=axes[ax_number]
+        ax_number+=1
+        empty_slice = np.array([np.nan]).repeat(array.shape[0])[:,np.newaxis]
+        slices=empty_slice
+        for s in slice_fractions:
+            slice=array[::-1,int(array.shape[1]*s),:]
+            slices=np.concatenate((slices,slice,empty_slice),axis=1)
+        pos = ax.imshow(slices, extent=[0,physical_dimensions[2]*num_slices,0,physical_dimensions[0]], vmin=vmin, vmax=vmax,cmap=cmap, alpha=alpha, interpolation='none')
+        ax.axis('off')
+        if cbar:
+            cbar_list.append(fig.colorbar(pos, ax=ax))
+
+    if 'horizontal' in planes:
+        ax=axes[ax_number]
+        ax_number+=1
+        empty_slice = np.array([np.nan]).repeat(array.shape[1])[:,np.newaxis]
+        slices=empty_slice
+        for s in slice_fractions:
+            slice=array[int(array.shape[0]*s),::-1,:]
+            slices=np.concatenate((slices,slice,empty_slice),axis=1)
+        pos = ax.imshow(slices, extent=[0,physical_dimensions[2]*num_slices,0,physical_dimensions[1]], vmin=vmin, vmax=vmax,cmap=cmap, alpha=alpha, interpolation='none')
+        ax.axis('off')
+        if cbar:
+            cbar_list.append(fig.colorbar(pos, ax=ax))
+    return cbar_list
