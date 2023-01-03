a6b63b99c45e90606d3f83c16e4f8b8870a58506
Jed Lamari-Saysset <jed.lamari-saysset@mail.mcgill.ca>
2023-01-03 08:47:59 -0500

    Updated animation with the working version.
    Works for single files and datasets. It will create a 3D animation of the fMRI using the 3 views.

--- Fall2022/animation.py
+++ Fall2022/animation.py
@@ -1,66 +1,17 @@
 '''
-
     Python script used to run the animation to help detect motion 
-
 '''
 
 import subprocess, argparse, sys, glob, os, csv
-import SimpleITK as sitk
 import nibabel as nb
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation, rc
+from IPython.display import HTML
+import SimpleITK as sitk
+#from rabies.visualization import plot_3d
 
-
-def animation(QPP_reshaped):
-    # example of plotting a 4D nifti across time, using the QPP image
-    # QPP_reshaped: a 4D nifti file loaded into a 2D time X voxel array
-
-    # the 3D voxel dimensions were transformed into a vector using the brain mask shape, so we need to retransform it into a 4D array
-    mask_file='mask.nii.gz' # brain mask
-    mask=np.asarray(nb.load(mask_file).dataobj)
-    shape=mask.shape
-    mask_vector=np.zeros([QPP_reshaped.shape[0],len(mask.reshape(-1))])
-    mask_indices=(mask.reshape(-1)==True)
-    data_array=np.zeros([QPP_reshaped.shape[0],shape[0],shape[1],shape[2]])
-    for i in range(QPP_reshaped.shape[0]):
-        mask_vector[i,mask_indices]=QPP_reshaped[i,:]
-        data_array[i,:,:,:]=mask_vector[i,:].reshape(shape)
-
-#Result: we have a 3D image now in data array
-    from nilearn.plotting import plot_stat_map
-    # First set up the figure, the axis, and the plot element we want to animate
-    import matplotlib.animation as animation
-    fig,ax = plt.subplots(nrows=1, ncols=1,figsize=(10,10))
-    plt.title('Coronal view', fontsize=30)
-    plt.axis('off')
-    vmax=np.abs(data_array).max()
-    vmin=-vmax
-
-    #instead of imshow, I need to find another funciton in RABIES
-
-    # initialization function: plot the background of each frame
-    def init():
-        line=ax.imshow(data_array[0,:,40,::-1].T, cmap='cold_hot',vmax=vmax,vmin=vmin)
-        return [line]
-
-    # animation function.  This is called sequentially
-    def animate(i):
-        line=ax.imshow(data_array[i,:,40,::-1].T, cmap='cold_hot',vmax=vmax,vmin=vmin)
-        return [line]
-
-    num_frames=10
-    # call the animator. blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames, #fargs=(QPP_reshaped),
-                                    interval=500, blit=True, repeat_delay=5000)
-
-    from matplotlib import animation, rc
-    from IPython.display import HTML
-    HTML(anim.to_html5_video())
-
-    #for IPython we shall pip install IPython
-    
-    
-    #from rabies.visualisation
+#from rabies.visualisation
 def plot_3d(axes,sitk_img,fig,vmin=0,vmax=1,cmap='gray', alpha=1, cbar=False, threshold=None, planes=('sagittal', 'coronal', 'horizontal'), num_slices=4, slice_spacing=0.1):
     physical_dimensions = (np.array(sitk_img.GetSpacing())*np.array(sitk_img.GetSize()))[::-1] # invert because the array is inverted indices
     array=sitk.GetArrayFromImage(sitk_img)
@@ -117,3 +68,98 @@ def plot_3d(axes,sitk_img,fig,vmin=0,vmax=1,cmap='gray', alpha=1, cbar=False, th
         if cbar:
             cbar_list.append(fig.colorbar(pos, ax=ax))
     return cbar_list
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='Motion animation stage of analysis', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('input_folder', 
+                        help="Input folder containing the subject to process. The folder can also be a dataset following\n"
+                             "the BIDS hierarchy with the fMRI data of each subject. If the folder is a dataset, specify\n"
+                             "the -d parameter. Each subject folder should respect the following format:\n"
+                             "   sub-0XX\n"
+                             "   └── ses-1\n"
+                             "       └── func\n"
+                             "           ├── _scan_info_subject_id0XX.session1.runNone_split_name_sub-0XX_ses-1_task-rest_acq-EPI_bold\n"
+                             "           │   └── sub-0XX_ses-1_task-rest_acq-EPI_bold_bold_ref.nii.gz\n"
+                             "           ├── sub-0XX_ses-1_func_sub-0XX_ses-1_task-rest_acq-EPI_bold.json\n"
+                             "           └── sub-0XX_ses-1_task-rest_acq-EPI_bold.nii.gz\n"
+                             "Here, XX represents the number of the subject\n")
+
+    parser.add_argument('output_folder', 
+                        help='Output folder')
+    parser.add_argument('-d', '--dataset', action='store_true', help='Folder provided is a dataset folder following the BIDS hierarchy')
+
+    return parser.parse_args()
+
+#Function convert the nifti into a numpy array
+def extractFile(path):
+    sitk_image = sitk.ReadImage(path, sitk.sitkFloat32)
+    return sitk_image
+
+#Function that defines the animation 
+def animationSubject(sitk_image, output_path):
+    fig,axes = plt.subplots(nrows=3, ncols=1,figsize=(20,10))
+    axes[0].set_title('Coronal view', fontsize=30, color='black')
+    axes[1].set_title('Saggital view', fontsize=30, color='black')
+    axes[2].set_title('Horizontal view', fontsize=30, color='black')
+    plt.axis('off')
+    
+    array = sitk.GetArrayFromImage(sitk_image)
+    vmax = array.max()
+    num_frames= int(array.shape[0]/5) 
+   
+    #instead of imshow(), we use the RABBIES function plot_3D
+    def init():
+        line = plot_3d(axes[:],sitk_image[:,:,:,0],fig=fig,vmin=0,vmax=vmax,cmap='viridis', cbar=False,planes=('sagittal', 'coronal', 'horizontal'), num_slices=4)
+        return line
+
+    # animation function. This is called sequentially
+    def animate(i):
+        line = plot_3d(axes[:],sitk_image[:,:,:,i*5],fig=fig,vmin=0,vmax=vmax,cmap='viridis', cbar=False,planes=('sagittal', 'coronal', 'horizontal'), num_slices=4)
+        return line
+
+    # call the animator
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames, interval=500, blit=True, repeat_delay=5000)
+
+    animation_output = os.path.join(output_path, "animation.mp4")
+    writer_anim = animation.FFMpegWriter(fps=30) 
+    anim.save(animation_output , writer=writer_anim)
+
+def animationMain(input_folder, output_folder, dataset):
+    #animate a single file
+    if not dataset:
+        moving = glob.glob(os.path.join(input_folder, "ses-1/func/*.nii.gz"))
+        if len(moving) == 0:
+            print(f"Failed to find the moving images nifti file in subject folder ses-1/func")
+            return
+        else: 
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+            animationSubject(extractFile(moving[0]), output_folder)
+    #animate an entire dataset
+    else:
+        all_subjects = glob.glob(os.path.join(input_folder, "sub-*/"))
+        if len(all_subjects) == 0:
+            print("No subjects found in dataset. Finishing job.")
+            return
+        for subject in all_subjects:
+            sub_name = subject.split('/')[-1] if subject.split('/')[-1] != '' else subject.split('/')[-2]
+            sub_num  = sub_name.split('-')[-1]
+            sub_output_folder = os.path.join(output_folder, sub_name + "/animation")
+            print(f"\n+ PROCESSING ANIMATION {sub_num} --------------------------------------------------------------+")
+            moving = glob.glob(os.path.join(subject, "ses-1/func/*.nii.gz"))
+            if len(moving) == 0:
+                print(f"Failed to find the moving images nifti file in subject folder {subject}/ses-1/func")
+                return 
+            if not os.path.exists(sub_output_folder):
+                    os.makedirs(sub_output_folder)
+            animationSubject(extractFile(moving[0]), sub_output_folder)
+
+
+if __name__ == "__main__":
+    args = parseArguments()
+    data = args.input_folder
+    output = args.output_folder
+    isDataSet = args.dataset
+    animationMain(data, output, isDataSet)
+
+
